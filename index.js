37ff255ec18dd646a8252803384989a896591c1d
alanleonelfernandez <alanleonelfernandez@hotmail.com>
2024-04-15 23:53:34 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+const { 
+    inquirerMenu, 
+    pausa, 
+    leerInput
+} = require('./helpers/inquirer');
+
+console.clear();
+
+const main = async() =>{
+    
+    let opt;
+
+    do{
+        opt = await inquirerMenu();
+        console.log({opt});
+
+        if(opt !== 0) await pausa();
+
+    }while( opt !== 0);
+}
+
+main();
